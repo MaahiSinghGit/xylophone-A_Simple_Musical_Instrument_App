27304999a47e3033e61ee9972ad573b2af536f88
Maahisinghgit <maahisingh07m@gmail.com>
2022-06-02 17:01:31 +0530

    Start

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() {
   runApp(const xylo());
 }
+
 class xylo extends StatelessWidget {
   const xylo({Key? key}) : super(key: key);
+  void playsound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            children: [
+              FlatButton(
+                color: Colors.red,
+                onPressed: () {
+                  playsound(1);
+                },
+                child: Text(""),
+              ),
+              FlatButton(
+                color: Colors.orange,
+                onPressed: () {
+                  playsound(2);
+                },
+                child: Text(" "),
+              ),
+              FlatButton(
+                color: Colors.yellow,
+                onPressed: () {
+                  playsound(3);
+                },
+                child: Text(" "),
+              ),
+              FlatButton(
+                color: Colors.green,
+                onPressed: () {
+                  playsound(4);
+                },
+                child: Text(" "),
+              ),
+              FlatButton(
+                color: Colors.teal,
+                onPressed: () {
+                  playsound(5);
+                },
+                child: Text(" "),
+              ),
+              FlatButton(
+                color: Colors.blue,
+                onPressed: () {
+                  playsound(6);
+                },
+                child: Text(" "),
+              ),
+              FlatButton(
+                color: Colors.purple,
+                onPressed: () {
+                  playsound(7);
+                },
+                child: Text(" "),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
-
